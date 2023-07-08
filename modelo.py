dfbf9b11057292d4e150febcc504eebab098af93
anisiabarros <anisia.barros@gmail.com>
2023-07-08 03:00:52 -0300

    encapsulando comportamentos e usando formatação em alguns metodos e atributos

--- modelo.py
+++ modelo.py
@@ -1,15 +1,51 @@
 class Filme:
     def __init__(self, titulo, ano, duracao):
-        self.titulo = titulo
+        self.__titulo = titulo.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+
+    @property
+    def likes(self):
+        return self.__likes
+
+
+    def dar_likes(self):
+        self.__likes += 1
+
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, novo_titulo):
+        self.__titulo = novo_titulo.title()
 
 
 class Serie:
     def __init__(self, titulo, ano, temporadas):
-        self.titulo = titulo
+        self.__titulo = titulo.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes +=1
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, novo_titulo):
+        self.__titulo = novo_titulo.title()
+
 
 
 vigadores = Filme("vigadores - guerra infinita", 2018, 160)
